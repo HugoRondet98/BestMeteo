a1c93b1e5a79d5a1da9759a6efe708840538a263
HugoRondet98 <hugo.rondet@yahoo.fr>
2024-06-14 14:20:54 +0200

    Initial Commit

---
+++ BestMeteo/BestMeteoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestMeteoApp.swift
+//  BestMeteo
+//
+//  Created by HugoRondet on 14/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct BestMeteoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
